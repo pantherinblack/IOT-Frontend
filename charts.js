eca08dfea9507811a50495eecf8e5c9c1bd05161
diego <fantinod@bzz.ch>
2023-04-26 15:38:20 +0200

    Stand 26.04.2023 (fixed y-Scale)

--- charts.js
+++ charts.js
@@ -1,4 +1,4 @@
-const url = "http://80.208.228.90:8080/record/list?time=120000";
+const url = "http://80.208.228.90:8080/record/list?time=86400";
 
 fetch(url, {
     method: 'GET',
@@ -12,42 +12,7 @@ fetch(url, {
         return response.json();
     })
     .then(data => {
-        /*
-        console.log(data)
-        console.log("recordUUID: " + data[0].recordUUID); // "cd01e9a0-955e-4ec2-8c32-5eac4e414f8c"
-        console.log("deviceUUID: " + data[0].deviceUUID); // "eui-a840417ee185f0b5"
-        console.log("timestamp: " + data[0].timestamp); // 1681386701000
-        console.log("temperature: " + data[0].temperature); // 15.7
-        console.log("humidity: " + data[0].humidity); // 38.2
-        console.log("battery: " + data[0].batteryv); // 3.663
-        console.log("deviceUUID: " + data[0].device.deviceUUID); // "eui-a840417ee185f0b5"
-        console.log("deviceName: " + data[0].device.deviceName); // "LSN50v2-30B"
-        console.log("device-latitude: " + data[0].device.latitude); // 47.35004
-        console.log("device-longitude: " + data[0].device.longitude); // 8.719297
-         */
         const chartData = {};
-
-        /*
-        const chartOptions = {
-            scales: {
-                xAxes: [{
-                    type: 'timestamp',
-                    time: {
-                        unit: 'minute'
-                    }
-                }],
-                yAxes: [{
-                    ticks: {
-                        beginAtZero: true
-                    }
-                }]
-            }
-        };
-        const chartConfig = {
-            type: 'line',
-            options: chartOptions
-        };
-         */
 
         var timestamps = [];
         var temperatures = [];
@@ -61,6 +26,16 @@ fetch(url, {
             batteryv.push(data[i].batteryv);
         }
 
+        // sort the arrays by timestamps in ascending order
+        var sortedIndex = timestamps.map((value, index) => [value, index])
+            .sort(([a], [b]) => a - b)
+            .map(([_, index]) => index);
+
+        timestamps = sortedIndex.map(index => timestamps[index]);
+        temperatures = sortedIndex.map(index => temperatures[index]);
+        humidity = sortedIndex.map(index => humidity[index]);
+        batteryv = sortedIndex.map(index => batteryv[index]);
+
         var ctx = document.getElementById('myChart').getContext('2d');
         var chart = new Chart(ctx, {
             type: 'line',
@@ -71,22 +46,19 @@ fetch(url, {
                     data: temperatures,
                     backgroundColor: 'rgba(255, 99, 132, 0.2)',
                     borderColor: 'rgba(255, 99, 132, 1)',
-                    borderWidth: 1,
-                    yAxisID: 'temperature'
+                    borderWidth: 1
                 }, {
                     label: 'Humidity (%)',
                     data: humidity,
                     backgroundColor: 'rgba(54, 162, 235, 0.2)',
                     borderColor: 'rgba(54, 162, 235, 1)',
-                    borderWidth: 1,
-                    yAxisID: 'humidity'
+                    borderWidth: 1
                 }, {
                     label: 'Battery Voltage (V)',
                     data: batteryv,
                     backgroundColor: 'rgba(255, 206, 86, 0.2)',
                     borderColor: 'rgba(255, 206, 86, 1)',
-                    borderWidth: 1,
-                    yAxisID: 'batteryv'
+                    borderWidth: 1
                 }]
             },
             options: {
@@ -106,78 +78,10 @@ fetch(url, {
                     yAxes: [{
                         scaleLabel: {
                             display: true,
-                            labelString: 'Temperature (C)'
+                            labelString: 'Temperature, Humidity, Battery Voltage'
                         }
                     }]
                 }
             }
         });
-
-
     });
-
-/* Old Code builds chart
-        // group data by deviceUUID
-        data.forEach(record => {
-            if (!chartData[record.deviceUUID]) {
-                chartData[record.deviceUUID] = {
-                    temperature: [],
-                    humidity: [],
-                    batteryv: [],
-                };
-            }
-            //console.log(chartData[record.deviceUUID].temperature);
-            chartData[record.deviceUUID].temperature.push({
-                x: new Date(record.timestamp  * 1000),
-                y: record.temperature
-            });
-            chartData[record.deviceUUID].humidity.push({
-                x: new Date(record.timestamp  * 1000),
-                y: record.humidity
-            });
-            chartData[record.deviceUUID].batteryv.push({
-                x: new Date(record.timestamp * 1000),
-                y: record.batteryv
-            });
-        });
-
-
-        // create a chart for each deviceUUID
-        //console.log(chartData);
-        Object.keys(chartData).forEach(deviceUUID => {
-            const canvas = document.createElement('canvas');
-            canvas.width = 400;
-            canvas.height = 400;
-            canvas.id = deviceUUID;
-            document.body.appendChild(canvas);
-
-            const ctx = canvas.getContext('2d');
-            chartConfig.data = {
-                datasets: [{
-                    label: 'Temperature',
-                    data: chartData[deviceUUID].temperature,
-                    borderColor: 'red',
-                    fill: false
-                }, {
-                    label: 'Humidity',
-                    data: chartData[deviceUUID].humidity,
-                    borderColor: 'green',
-                    fill: false
-                }, {
-                    label: 'Batteryv',
-                    data: chartData[deviceUUID].batteryv,
-                    borderColor: 'blue',
-                    fill: false
-                }]
-            };
-            //console.log(ctx)
-            new Chart(ctx, chartConfig);
-        });
-    })
-    .catch(error => {
-        console.error(error);
-         */
-
-
-
-
