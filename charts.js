6d937c10fc783496b9027bc8ae7ba3e6740993ad
diego <fantinod@bzz.ch>
2023-04-27 14:35:12 +0200

    Stand 27.04.2023 (edited button configuration to change between a day, week, month and year)

--- charts.js
+++ charts.js
@@ -1,5 +1,32 @@
-const url = "http://80.208.228.90:8080/record/list?time=86400";
+document.addEventListener("DOMContentLoaded", () => {
+const url = "http://80.208.228.90:8080/record/list?time=31";
 
+    const dropdown = document.getElementById("choose-device");
+    console.log(dropdown);
+
+// Fills Dropdown with devices
+fetch("http://80.208.228.90:8080/device/list", {
+    method: 'GET',
+    headers: {'Content-Type': 'application/json'}
+}).then(response => {
+    if (!response.ok) {
+        throw new Error("HTTP error " + response.status);
+    }
+    return response.json();
+}).then(data => {
+    data.forEach(device => {
+        const option = document.createElement("option");
+        option.text = device.deviceName;
+        option.value = device.deviceUUID;
+        console.log(device.deviceUUID);
+        dropdown.add(option);
+    });
+}).catch(error => {
+    console.error(error);
+});
+
+
+// Creates Chart with Standard last Month
 fetch(url, {
     method: 'GET',
     headers: {'Content-Type': 'application/json'}
@@ -50,7 +77,8 @@ fetch(url, {
             });
             timestamps[i] = formattedDate;
         }
-
+        // diasble Month button
+        document.getElementById("lastMonthBtn").disabled = true;
         var ctx = document.getElementById('myChart').getContext('2d');
         var chart = new Chart(ctx, {
             type: 'line',
@@ -162,7 +190,6 @@ function updateChartData(updateurl) {
 
 
 
-document.addEventListener("DOMContentLoaded", function() {
     // Get the buttons by their ID
     const lastDayBtn = document.getElementById("lastDayBtn");
     const lastWeekBtn = document.getElementById("lastWeekBtn");
@@ -172,25 +199,37 @@ document.addEventListener("DOMContentLoaded", function() {
     // Add event listeners to each button
     lastDayBtn.addEventListener("click", function() {
         // Handle click for "Last Day" button
-        console.log("last Day");
-        updateChartData("http://80.208.228.90:8080/record/list?time=86400");
+        lastDayBtn.disabled = true;
+        lastWeekBtn.disabled = false;
+        lastMonthBtn.disabled = false;
+        lastYearBtn.disabled = false;
+        updateChartData("http://80.208.228.90:8080/record/list?time=1");
     });
 
     lastWeekBtn.addEventListener("click", function() {
         // Handle click for "Last Week" button
-        console.log("last Week");
-        updateChartData("http://80.208.228.90:8080/record/list?time=604800");
+        lastDayBtn.disabled = false;
+        lastWeekBtn.disabled = true;
+        lastMonthBtn.disabled = false;
+        lastYearBtn.disabled = false;
+        updateChartData("http://80.208.228.90:8080/record/list?time=7");
     });
 
     lastMonthBtn.addEventListener("click", function() {
         // Handle click for "Last Month" button
-        console.log("last Month");
-        updateChartData("http://80.208.228.90:8080/record/list?time=2678400");
+        lastDayBtn.disabled = false;
+        lastWeekBtn.disabled = false;
+        lastMonthBtn.disabled = true;
+        lastYearBtn.disabled = false;
+        updateChartData("http://80.208.228.90:8080/record/list?time=31");
     });
 
     lastYearBtn.addEventListener("click", function() {
         // Handle click for "Last Year" button
-        console.log("last Year");
-        updateChartData("http://80.208.228.90:8080/record/list?time=31536000");
+        lastDayBtn.disabled = false;
+        lastWeekBtn.disabled = false;
+        lastMonthBtn.disabled = false;
+        lastYearBtn.disabled = true;
+        updateChartData("http://80.208.228.90:8080/record/list?time=365");
     });
 });
